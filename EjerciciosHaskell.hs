da4517d762ca02310996e06446f26809186e6936
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2024-05-26 14:19:39 -0700

    Ejercicios de practica

---
+++ EjerciciosHaskell.hs
@@ -0,0 +1,22 @@
+-- Ejercicios
+
+-- Calcular todos los triangulos pitagoricos con lados menores a 10
+-- a^2 + b^2 = c^2
+-- a < b < c
+
+
+
+-- Calcular el factorial de un número
+-- factorial 5 = 5 * 4 * 3 * 2 * 1 = 120
+
+
+
+
+-- Calcular la suma de los primeros 100 números naturales
+-- 1 + 2 + 3 + ... + 100 = 5050
+
+
+
+-- Realizar una función que reciba una lista de números y devuelva la suma de los elementos pares de la lista
+-- sumarPares [1,2,3,4,5,6,7,8,9,10] = 2 + 4 + 6 + 8 + 10 = 30
+-- si no tiene par, devolver 0
